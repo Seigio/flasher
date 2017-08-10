21c66234928eb23073fae12f4cce4960d1f22c63
Seigio <charles.kearney@outlook.com>
2017-08-10 10:58:20 -0400

    Probably too many comments

--- static/Deck.js
+++ static/Deck.js
@@ -1,17 +1,38 @@
-//Each deck is made of multiple flashcards, which contain a title, a question, and an answer
+/*
+    Deck is a Module used to hold and access Flashcard objects.  Each Flashcard contains three* properties
+    (title, answer, and question) instantiated from a json object.  The json file is a representation of 
+    a Deck Module, containing json objects representing each individual Flashcard.  Deck, and its
+    children Flashcards, are all instantiated from a single json file.
+
+    Flashcards are stored in the flashcards[] array.  To access a Flashcard property, the Flashcard is 
+    retrieved from the array by index, and the get function for the required property is called on the
+    retrieved Flashcard.
+
+    Deck is a Module with the following functions and variables:
+        private:
+            flashcards[]: array - Holds all the Flashcard objects in the Deck
+        
+        public:
+            init_from_json():   void - Intializes the Deck Module from a json file
+            add_flashcard():    void - Pushes a Flashcard to the flashcards[] array
+            remove_flashcard(): void - Removes a Flashcard from the flashcards[] array
+            get_length():       void - Gets the length of the flashcards[] array
+            get_flashcard():    void - Gets a Flashcard from the flashcards[] array
+            get_title():        void - Gets the title of a Flashcard
+            get_question():     void - Gets the question text of a Flashcard
+*/
 var Deck = (function($){
-    //array of flashcard objects
     var flashcards = [];
 
     /*
-        Creates a flashcard module accessible only from within the Deck module
+        Creates a Flashcard module accessible only from within the Deck module
         Flashcard properties can only be instantiated from the init_from_json function
         Properties:
+            private:
+                string: title, question, answer;
             public:
                 string: get_title(), get_question(), get_answer();
                 void:   init_from_json();
-            private:
-                string: title, question, answer;
                 
     */
     var Flashcard = function(){
@@ -43,25 +64,20 @@ var Deck = (function($){
         };
     };
 
-    function get_flashcard(index){
-        if (index >= flashcards.length || index < 0){
-            return;
-        } else {
-            return flashcards[index];
+
+    /*------------------------------------------------------------------------------------------------
+       Functions for manipulating the flashcards[] array
+    ------------------------------------------------------------------------------------------------*/
+    function init_from_json(json){
+        var i = 0;
+        var n_flashcard;
+        for(var q in json){
+            //not sure if this will work, initializes a new flashcard (n_flashcard) from a json object
+            n_flashcard = Flashcard();
+            n_flashcard.init(json[q]);
+            add_flashcard(n_flashcard);
         }
     }
-
-    /*
-        Checks an input answer against the correct answer
-        Paramaters:       int: index        u_answer (user answer): string
-        Returns:          boolean: true if answer matches, false otherwise
-    */          
-    function check_answer(index, u_answer){
-        var answer = get_flashcard(index).get_answer();
-        
-        return (answer.toLowerCase() === u_answer.toLowerCase());
-    }
-
     //The publish functionality was to see if I could
     //to-do, refactor this to works better with selecting a deck
     function add_flashcard(flashcard){
@@ -71,6 +87,22 @@ var Deck = (function($){
 
     function remove_flashcard(index){
         flashcards.splice(index, 1);
+    }
+    
+    function get_length(){
+        return flashcards.length;
+    }
+    
+
+    /*------------------------------------------------------------------------------------------------
+       Functions for accessing Flashcards and Flashcard properties from the flashcards[] array
+    ------------------------------------------------------------------------------------------------*/
+    function get_flashcard(index){
+        if (index >= flashcards.length || index < 0){
+            return;
+        } else {
+            return flashcards[index];
+        }
     }
 
     function get_title(index){
@@ -85,22 +117,21 @@ var Deck = (function($){
         return get_flashcard(index).answer();
     }
 
-    function init_from_json(json){
-        var i = 0;
-        var n_flashcard;
-        for(var q in json){
-            //not sure if this will work, initializes a new flashcard (n_flashcard) from a json object
-            n_flashcard = Flashcard();
-            n_flashcard.init(json[q]);
-            add_flashcard(n_flashcard);
-        }
+    /*
+        Checks an input answer against the correct answer
+        Paramaters:       int: index        u_answer (user answer): string
+        Returns:          boolean: true if answer matches, false otherwise
+    */          
+    function check_answer(index, u_answer){
+        var answer = get_flashcard(index).get_answer();
+        
+        return (answer.toLowerCase() === u_answer.toLowerCase());
     }
 
-    function get_length(){
-        return flashcards.length;
-    }
 
-    return{
+
+
+    return {
         add: add_flashcard,
         remove: remove_flashcard,
         get: get_flashcard,
