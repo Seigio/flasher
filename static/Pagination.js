5f1ae986b35f422a61b30e2fb9df880cdf6eab9e
Seigio <charles.kearney@outlook.com>
2017-08-10 10:30:11 -0400

    Changing init to be accessible outside the function, adding comments

--- static/Pagination.js
+++ static/Pagination.js
@@ -1,14 +1,19 @@
-var Pagination = (function($){
+
+
+var Pagination = (function($){ //function($) passes in the jQuery module to be used within the function
     var current_page, total_pages;
 
-    var init = (function(){
+
+    var init = function(){
         current_page = 1;
         total_pages = 0;
+        //Updates total_pages whenever a new card is added to the current deck
         $.subscribe('card-added', function(){
             total_pages++;
         });
-    })();
+    };
 
+    //Updates the DOM whenever the current page is changed
     function update_page(){
         console.log(current_page + "    " + total_pages);
         $('#question_header').text(Deck.title(current_page - 1));
@@ -31,6 +36,7 @@ var Pagination = (function($){
         previous: function(){
             return go_to(current_page - 1);
         },
-        go_to: go_to
+        go_to: go_to,
+        init: init
     };
 })(jQuery);
